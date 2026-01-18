7449e552b4d457c7acae60e68f6932d1e1e0e8f5
Bertini <bertin_p@etna-alternance.net>
2026-01-18 17:44:14 +0100

    variables (str, int, float, bool)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello, Lola!")
+# INT / INTEGER
+# ex : 1, -4, 0
 
+# FLOAT
+# ex : 1.5, -0.3, 0.0, 3.0, 2.000001
 
-main()
+# STR / STRING
+# ex : "hello", 'world', "123", "", ' '
+
+# BOOL / BOOLEAN
+# ex : True, False
